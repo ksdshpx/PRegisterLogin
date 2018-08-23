f4a30d4386e5c47a1f53cdbdcc2d27fac504dc8f
ksdshpx <southeast_px@163.com>
2018-08-23 15:06:14 +0800

    项目加入jdbc代码！

---
+++ src/main/java/cn/ksdshpx/user/dao/JdbcUserDaoImpl.java
@@ -0,0 +1,76 @@
+package cn.ksdshpx.user.dao;
+
+import cn.ksdshpx.user.domain.User;
+import cn.ksdshpx.user.utils.JdbcUtils;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+/**
+ * Create with IntelliJ IDEA
+ * Create by peng.x
+ * Date: 2018/8/23
+ * Time: 14:50
+ * Description:加入Jdbc代码
+ */
+public class JdbcUserDaoImpl implements UserDao {
+
+    /**
+     * 按用户名查询
+     *
+     * @param username
+     * @return
+     */
+    public User findByUsername(String username) {
+        Connection conn = null;
+        PreparedStatement pstmt = null;
+        ResultSet rs = null;
+        User user = null;
+        try {
+            conn = JdbcUtils.getConnection();
+            String sql = "SELECT * FROM t_user WHERE username = ?";
+            pstmt = conn.prepareStatement(sql);
+            pstmt.setString(1, username);
+            rs = pstmt.executeQuery();
+            if (rs.next()) {
+                user = new User();
+                user.setUsername(rs.getString("username"));
+                user.setPassword(rs.getString("password"));
+                user.setAge(rs.getInt("age"));
+                user.setGender(rs.getString("gender"));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            JdbcUtils.close(conn, pstmt, rs);
+        }
+        return user;
+    }
+
+    /**
+     * 添加用户
+     *
+     * @param user
+     */
+    public void add(User user) {
+        Connection conn = null;
+        PreparedStatement pstmt = null;
+        ResultSet rs = null;
+        try {
+            conn = JdbcUtils.getConnection();
+            String sql = "INSERT INTO t_user(username,password,age,gender) VALUES(?,?,?,?)";
+            pstmt = conn.prepareStatement(sql);
+            pstmt.setString(1, "zhangSan");
+            pstmt.setString(2, "123");
+            pstmt.setInt(3, 25);
+            pstmt.setString(4, "male");
+            pstmt.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            JdbcUtils.close(conn, pstmt, rs);
+        }
+    }
+}
--- src/main/java/cn/ksdshpx/user/domain/User.java
+++ src/main/java/cn/ksdshpx/user/domain/User.java
@@ -10,15 +10,18 @@ package cn.ksdshpx.user.domain;
 public class User {
     private String username;
     private String password;
+    private Integer age;
+    private String gender;
     private String verifyCode;
 
     public User() {
     }
 
-    public User(String username, String password, String verifyCode) {
+    public User(String username, String password, Integer age, String gender) {
         this.username = username;
         this.password = password;
-        this.verifyCode = verifyCode;
+        this.age = age;
+        this.gender = gender;
     }
 
     public String getUsername() {
@@ -37,6 +40,22 @@ public class User {
         this.password = password;
     }
 
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
     public String getVerifyCode() {
         return verifyCode;
     }
@@ -50,6 +69,8 @@ public class User {
         return "User{" +
                 "username='" + username + '\'' +
                 ", password='" + password + '\'' +
+                ", age=" + age +
+                ", gender='" + gender + '\'' +
                 ", verifyCode='" + verifyCode + '\'' +
                 '}';
     }
